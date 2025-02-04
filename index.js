529963126b9c3fde97aee2a9bd7a581c92c033fd
Jailton Brito <jailton.dev.br@gmail.com>
2025-02-04 20:07:16 -0300

    Atualizada As Mensagens Do Sistema

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { turbologger } from "./turbologger.js";
 
 console.log("Estou usando o Github");
 turbologger("Estou aprendendo O Fluxo do Git", "red");
+turbologger(
+  "Eu consegui criar uma Nova Mensagem Sem Afetar a Versão Principal do Sistema",
+  "darkgreen"
+);
